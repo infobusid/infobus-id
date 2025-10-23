cfecb737280d42e69baa4c5abcbeb22f8e5d5168
infobusid <informasibus.id@gmail.com>
2025-10-23 08:48:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
-from app import app
+from flask import Flask
 
 app = Flask(__name__)
 
-# Data user (dalam contoh ini, kita menggunakan data dummy)
-users = {
-    'user1': 'password1',
-    'user2': 'password2'
-}
+@app.route('/')
+def index():
+    return 'Halo, Dunia!'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username in users and users[username] == password:
-            return redirect(url_for('dashboard'))
-        else:
-            return 'Username atau password salah'
-    return render_template('index.html')
-
-@app.route('/dashboard')
-def dashboard():
-    return 'Selamat datang di dashboard!'
-
-if __name__ == '__main
+if __name__ == '__main__':
+    app.run(debug=True)
